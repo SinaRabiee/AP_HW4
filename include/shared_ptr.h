df633c5c1bc51d4ced5ea30e9b023d7e8e8a0bf1
root <sinarabiee80@gmail.com>
2022-04-27 11:30:57 +0000

    header files completed and build folder ignored

--- include/shared_ptr.h
+++ include/shared_ptr.h
@@ -1,5 +1,32 @@
 #ifndef SHARED_PTR
 #define SHARED_PTR
+#include <iostream>
+#include <utility>
 
+template <typename T>
+class SharedPtr {
+public:
+    SharedPtr(T* arr);
+    SharedPtr();
+    ~SharedPtr();
+    SharedPtr(const SharedPtr& Ptr);
+    SharedPtr<T>& operator=(const SharedPtr& Ptr);
+    size_t use_count();
+    T* get();
+    T& operator*();
+    T* operator->();
+    void reset();
+    void reset(T* arr);
+    T* release();
+    operator bool() const;
 
-#endif //SHARED_PTR
+private:
+    T* _p;
+    int* count;
+};
+
+template <typename T, typename... U>
+T* make_shared(U&&... args);
+
+#include "shared_ptr.hpp"
+#endif // SHARED_PTR
--- include/unique_ptr.h
+++ include/unique_ptr.h
@@ -1,14 +1,31 @@
 #ifndef UNIQUE_PTR
 #define UNIQUE_PTR
+#include <iostream>
+#include <memory>
+#include <utility>
 
+template <typename T>
+class UniquePtr {
+public:
+    UniquePtr(T* arr);
+    UniquePtr();
+    ~UniquePtr();
+    UniquePtr(const UniquePtr& Ptr) = delete;
+    UniquePtr<T>& operator=(const UniquePtr& Ptr) = delete;
+    T* get();
+    T& operator*();
+    T* operator->();
+    void reset();
+    void reset(T* arr);
+    T* release();
+    operator bool() const;
 
+private:
+    T* _p;
+};
 
+template <typename T, typename... U>
+T* make_unique(U&&... args);
 
-
-
-
-
-
-
-
-#endif //UNIQUE_PTR
+#include "unique_ptr.hpp"
+#endif // UNIQUE_PTR
